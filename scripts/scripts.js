cef52790efc940a6048c8228ceda577adbfd25cd
Morris <morris9510@gmail.com>
2023-11-08 19:44:22 -0600

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,79 @@
+console.log("virus");
+
+//Creating string var
+let myName = "morris";
+
+let lastName = "Jacobs";
+let myAge = "23";
+let mySize = "67";
+// creating number var
+let pricetag = 99.75;
+
+//creating boolean var
+let isProfessor = true;
+let isStudent = false;
+
+console.log(myName);
+console.log(lastName);
+console.log(price);
+
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+let division = num2 / num1;
+let mult = num1 * num2;
+var nickName;
+const Kids = 10;
+console.log(Kids);
+let Animal = "What animal do you have";
+
+var middleName = "What is your nickname?";
+var nickName = "What is your nickname";
+var verb = "run";
+var price = "Total";
+var tax = price + "$" + 4.5;
+var food = "What is your favorite food";
+var apps = "what is your favorite app";
+var design = "what is your favorite design";
+var code = " what is your favorite coding language";
+let weather = "what is the weather like near you";
+const settlement = "bankruptcy";
+var imessage = "who sends you the most imessages";
+var photos = " show us your favorite photo";
+var file = "how do you send files";
+var games = "whats your favorite game";
+var shoppingCart = "what is in your cart";
+var printLabel = "printing labels";
+var emailList = "email" + 300;
+var keyboardChart = 48;
+var calendar = "365";
+var caculator = "math";
+
+document.write(`
+<p>Food: ${food}</p>
+<p>nickName: ${nickName}</p>
+<p>verb is ${verb}</p>
+<p>tax  ${tax}</p>
+<p>food: ${food}</p>
+<p>apps${apps}</p>
+<p>Name: ${myName}</p>
+<p>caculator ${caculator}</p>
+<p> weather ${weather}</p>
+<p>keyboardChart ${keyboardChart}</p>
+<p>calendar ${calendar}</p>
+<p>printLabel ${price}</p>
+<p>emailList ${emailList}</p>
+<p>shoppingCart ${shoppingCart}</p>
+<p>design${design}</p>
+<p>imessage ${imessage}</p>
+<p>photos ${photos}</p>
+<p>Price is ${price}</p>
+<p>middleName ${middleName}</p>
+<p>Age: ${myAge}</p>
+
+
+
+
+
+`);
---
+++ scripts/sum.js
@@ -0,0 +1,26 @@
+function sum() {
+  let a = prompt("Enter the first value (a):");
+
+  let b = prompt("enter the second value(b):");
+
+  let total;
+  total = Number(a) + Number(b);
+
+  console.log(total);
+}
+
+function mult() {
+  let a = prompt("Enter a value here(a):");
+
+  let b = 5;
+
+  totalMult = Number(a) * b;
+
+  console.log(totalMult);
+}
+
+function convert() {
+  let temperature = Number(prompt("Enter the temperature in Celsius"));
+  temperature = (temperature * 9) / 5 + 32;
+  console.log(temperature);
+}
