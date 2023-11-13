defb1f65660e0744679d9eb083aa25667a578c26
Morris <morris9510@gmail.com>
2023-11-12 19:16:25 -0600

    homework final report

--- scripts/sum.js
+++ scripts/sum.js
@@ -11,7 +11,7 @@ function convertTemperature() {
   let scale = prompt("Enter the scale (C or F)");
   if (scale === "C") {
     start = (start * 9) / 5 + 32;
-    console.log(start + " °F");
+    console.log(start + " F");
   } else if (scale === "F") {
     start = ((start - 32) * 5) / 9;
     console.log(start + " C");
@@ -19,3 +19,25 @@ function convertTemperature() {
     console.log("Use celsius or Fahrenheit");
   }
 }
+
+//final report
+
+function convertTemperatureRange(startValue, endValue, scale) {
+  for (let temp = startValue; temp <= endValue; temp++) {
+    if (scale === "C") {
+      let convertedTemp = Math.round((temp * 9) / 5 + 32);
+      console.log(convertedTemp + " F");
+    } else if (scale === "F") {
+      let convertedTemp = Math.round(((temp - 32) * 5) / 9);
+      console.log(convertedTemp + " C");
+    } else {
+      console.log("use celsius or fahrenehit");
+    }
+  }
+}
+
+let startTemperature = Number(prompt("Enter starting temperature:"));
+let endTemperature = Number(prompt("Enter ending temperature:"));
+let scale = prompt("Scale:  C or F");
+
+convertTemperatureRange(startTemperature, endTemperature, scale);
